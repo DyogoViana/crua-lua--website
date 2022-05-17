84b437c73a9b5b7f80625fe24a962988aef12102
DyogoViana <dyogo.vianaa@gmail.com>
2022-05-17 11:35:58 -0300

    [Ajuste] : : prettier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-	return <h1>xxx</h1>;
+	return <h1>main</h1>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import ResetCSS from './generic/resetCSS';
-import Base from './styles/elements/Base';
+import App from "./App.js";
+import Header from "./components/Header/header.js";
+import ResetCSS from "./generic/resetCSS.js";
+import Base from "./styles/elements/Base.js";
 
-import Header from './components/Header/header'
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ResetCSS />
-    <Base />
-	 <Header />
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<ResetCSS />
+		<Base />
+		<Header>Header</Header>
+		<App />
+	</React.StrictMode>
 );
